2760d7d8d4cf6ed40a452a3629929f78ad7dfc15
Vooskie <pierce@baronoff.net>
2021-12-14 14:16:01 +1000

    Update Log4JPatcher.bat

--- Latest/Log4JPatcher.bat
+++ Latest/Log4JPatcher.bat
@@ -5,26 +5,55 @@
 @echo #   visit pierce.baronoff.net for more tools   #
 @echo #                                              #
 @echo ################################################
+set /p vrsn=1.5
+cd c:\
 timeout 5 > NUL
+@echo Cehcking for updates
+timeout 1 > NUL
+set url=http://pierce.baronoff.net/Log4jPatcher/version.txt
+set file=version.txt
+certutil -urlcache -split -f %url% %file%
+set /p nwstvrsn=<version.txt
+if %vrsn% < %nwstvrsn% goto newupdateavailable
+if %vrsn% == %nwstvrsn% goto noupdateavailable
+:newupdateavailable
+echo Downloading Update
+set url=http://pierce.baronoff.net/Log4J%20Patcher/Latest/Log4JPatcher.bat
+set file=Log4JPatcher.bat
+
+certutil -urlcache -split -f %url% %file%
+call Log4J%20Patcher%20V%nwstvrsn%.bat
+
+:noupdateavailable
+echo No Update Available
+timeout 1 > NUL
 setx LOG4J_FORMAT_MSG_NO_LOOKUPS true /m
 timeout 1 > NUL
 if "%LOG4J_FORMAT_MSG_NO_LOOKUPS%" equ "true" (
   
    echo Patching was successful
    timeout 2 > NUL
-   GOTO Confirmation
+   GOTO CheckReboot
 
 ) else (
 
    echo Patching was not successful
    timeout 5 > NUL
-   @echo Please check that you are running as administrator from C: drive and try again
+   @echo Please check that you are running as administrator from C:\ drive and try again
    timeout 2 > NUL
    GOTO EOF
 
 )
 timeout 5 > NUL
 GOTO EOF
+
+    :CheckReboot
+    
+    SET /P yesno=Do you want to Reboot this machine? [Y/N]:
+    IF "%yesno%"=="y" GOTO Confirmation
+    IF "%yesno%"=="Y" GOTO Confirmation
+    IF "%yesno%"=="n" GOTO End
+    IF "%yesno%"=="N" GOTO End
     
     :Confirmation
     
